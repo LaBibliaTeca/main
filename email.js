3542a4bd6c34a098a4023c48ec83ad1c0255fc42
LaBibliaTeca <gustavo.uliarte@hotmail.com>
2024-12-30 13:31:57 -0300

    Update email.js

--- email.js
+++ email.js
@@ -24,14 +24,3 @@ script.onload = function() {
 };
 
 
-        // Botón para enviar por WhatsApp
-        document.getElementById('sendWhatsapp').addEventListener('click', function() {
-            const message = document.getElementById('message').value;
-            if (!message) {
-                alert('Por favor, escribe un mensaje.');
-                return;
-            }
-            const phoneNumber = '541156394763'; // Número en formato internacional
-            const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-            window.open(whatsappLink, '_blank');
-        });
