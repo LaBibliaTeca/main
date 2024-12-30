49331ecdf95abc747563b7c81bffb8fefaeca47f
LaBibliaTeca <gustavo.uliarte@hotmail.com>
2024-12-30 14:25:19 -0300

    Update wame.js

--- wame.js
+++ wame.js
@@ -1,15 +1,14 @@
-document.getElementById('sendWhatsapp').addEventListener('click', function() {
-    const message = document.getElementById('message').value.trim();
-    if (!message) {
-        alert('Por favor, escribe un mensaje.');
-        return;
-    }
+        document.getElementById('sendWhatsapp').addEventListener('click', function(event) {
+            event.preventDefault();
 
-    const phoneNumber = '1234567890'; // Número en formato internacional
-    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+            // Obtener los datos del formulario
+            var nombre = document.querySelector('input[name="from_name"]').value;
+            var mensaje = document.querySelector('textarea[name="name="message"]').value;
 
-    // Forzar apertura del enlace
-    // location.href = whatsappLink;
-    window.open(whatsappLink, '_blank');
+            // Construir el enlace para WhatsApp Web
+            var whatsappURL = "https://web.whatsapp.com/send?phone=541156394763&text=" + encodeURIComponent("Mensaje de: " + nombre + "\n" + mensaje);
 
-});
+            // Redirigir al usuario a WhatsApp Web
+            window.open(whatsappURL, '_blank');
+        });
+    </script>
